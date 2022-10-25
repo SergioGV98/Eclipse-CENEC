b97b1b310553f6c48e314eee12d15270332b49ea
Sergio García Vico <sergiogarciavicooficial@gmail.com>
2022-10-25 18:23:55 +0200

    Sergio

--- Tema 3/EstructuraControlFlujo/src/main/java/controlflujo/sergio/ControlFlujo.java
+++ Tema 3/EstructuraControlFlujo/src/main/java/controlflujo/sergio/ControlFlujo.java
@@ -9,6 +9,19 @@ public class ControlFlujo {
 		//Scanner
 		Scanner sc = new Scanner(System.in);
 
+		//Variables
+		
+		
+		//Programa
+		System.out.println("Escribeme una palabra que no sea \"Cermuzo\"");
+		String palabra = sc.nextLine();
+		
+		if(palabra.length() > 8) {
+			System.out.println("No me gustan las palabras tan largas.");
+		} else if(palabra.equals("cermuzo") || palabra.equals("Cermuzo")) {
+			System.out.println("Has escrito cermuzo, no escribas cermuzo.");
+		} else {
+			System.out.println("Bonita palabra.");
+		}
+		}	
 	}
-
-}
---
+++ Tema 3/HoraDeAventura/src/main/java/horadeaventura/sgv/HoraDeAventura.java
@@ -0,0 +1,80 @@
+package horadeaventura.sgv;
+
+import java.util.Scanner;
+
+public class HoraDeAventura {
+
+	public static void main(String[] args) {
+
+		//Scanner
+		Scanner sc = new Scanner(System.in);
+		
+		//Variables
+		byte opcionElegida;
+		byte opcionElegida2 = 0;
+		byte opcionElegida3 = 0;
+		
+		//Empieza la aventura
+		System.out.println("Estamos en CENEC un pacifico dia en clase." +
+		"\n Y de repente, cae del tragaluz" + "una jirafa con gafas de sol y manos, \n" + 
+		"Una botella de vodka en una mano y una bandurria en la otra mano" +
+		"\n ¿Como reaccionas?" +
+		//Eleccion 01
+		"\n\t1 - Decirle: ¡Vamonos de fiesta!" 
+		+ "\n\t2 - Intentar robarle el vozkza"
+		+ "\n\t3 - Pegarle con la bandurria en la cabeza");
+		
+		//Opcion elegida 1
+		opcionElegida=Byte.parseByte(sc.nextLine());
+		
+		if(opcionElegida == 1) {
+			System.out.println("Jirafa: " + "¿De fiesta?");
+			System.out.println("Jirafa: " + "Me acabo de caer por un tragaluz, estoy molida. \n" +
+			"Claro, ves una jirafa con vodka y te quieres ir de fiesta, \n" +
+			"pero nadie pregunta cómo está la jirafa después de caerse.");
+			
+			//Opcion elegida 1 Eleccion 2
+			System.out.println("¿Qué haces?" + 
+					"\n\t1 - Jirafa tia, lo siento, ¿Estás bien?" 
+					+ "\n\t2 - ¿Y tu me has preguntado como estoy yo?");
+			
+			opcionElegida2=Byte.parseByte(sc.nextLine());
+				 
+			if(opcionElegida2 == 1) {
+				System.out.println("Jirafa: " + "¿Puedes llamar a una ambulancia?");
+			} else {
+				System.out.println("Porque yo necesito irme de fiesta.");
+				System.out.println("Jirafa: " + "Pues toma el vodka y vete.\n" + 
+				"*Te tira el vodka a la cara*\n" + 
+			    "*Te vas de fiesta con la nariz rota por un vodkazo.*");
+			}
+		}
+
+		//Opcion elegida 2
+		if(opcionElegida == 2) {
+			System.out.println("Jirafa: " + "Es mi vodka, mequetrefe.\n" + 
+		    "*La girafa te da un cabezado*");
+			
+			//Opcion elegida 2 Eleccion 2
+			System.out.println("¿Qué haces?" + 
+					"\n\t1 - Le hago un RKO" 
+					+ "\n\t2 - Salgo corriendo.");
+			
+			opcionElegida2=Byte.parseByte(sc.nextLine());
+				 
+			if(opcionElegida2 == 1) {
+				System.out.println("*Le haces un RKO, increible y le partes el cuello a la jirafa.*");
+			} else {
+				System.out.println("*Sales corriendo y la jirafa te persige, acabas muriendo de un infarto por correr tanto*");
+			}
+		}
+		
+		//Opcion elegida 3
+		if(opcionElegida == 3) {
+			System.out.println("*Le metes con la bandurria en la cabeza y decides hacerte /n" + 
+		"el mejor solo de la historia por tu amor a la pelicula Whiplash");
+		}
+		
+	}
+
+}
