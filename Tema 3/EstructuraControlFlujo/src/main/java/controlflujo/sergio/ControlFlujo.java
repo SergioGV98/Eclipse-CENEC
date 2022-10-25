c7c7bcea7dca6b9ca5e2622a822d524f26baa0dd
Sergio García Vico <sergiogarciavicooficial@gmail.com>
2022-10-25 15:47:19 +0200

    Sergio

---
+++ Tema 3/EstructuraControlFlujo/src/main/java/controlflujo/sergio/ControlFlujo.java
@@ -0,0 +1,14 @@
+package controlflujo.sergio;
+
+import java.util.Scanner;
+
+public class ControlFlujo {
+
+	public static void main(String[] args) {
+		
+		//Scanner
+		Scanner sc = new Scanner(System.in);
+
+	}
+
+}
