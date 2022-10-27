052ea5667f368b7f552701c05ec513a7993ec144
Sergio García Vico <sergiogarciavicooficial@gmail.com>
2022-10-27 15:43:08 +0200

    Sergio

--- Tema 3/EstructuraControlFlujo/src/main/java/controlflujo/sergio/ControlFlujo.java
+++ Tema 3/EstructuraControlFlujo/src/main/java/controlflujo/sergio/ControlFlujo.java
@@ -12,6 +12,51 @@ public class ControlFlujo {
 		//Variables
 		
 		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		/*System.out.println("Escribeme H si eres hombre o " +
+		"M si eres mujer");
+		
+		boolean genero = (sc.nextLine().equals("H")?true:false);
+		
+		if(sc.nextLine().equals("H")) {
+			System.out.println("Eres hombre.");
+			genero=true;
+		} else {
+			System.out.println("Eres mujer.");
+			genero=false;
+		}
+		
 		//Programa
 		System.out.println("Escribeme una palabra que no sea \"Cermuzo\"");
 		String palabra = sc.nextLine();
@@ -19,9 +64,9 @@ public class ControlFlujo {
 		if(palabra.length() > 8) {
 			System.out.println("No me gustan las palabras tan largas.");
 		} else if(palabra.equals("cermuzo") || palabra.equals("Cermuzo")) {
-			System.out.println("Has escrito cermuzo, no escribas cermuzo.");
+			System.out.println("Has escrito cermuzo, no escribas cermuzo, so cermuz" + (genero?"o":"a"));
 		} else {
 			System.out.println("Bonita palabra.");
-		}
+		} */
 		}	
 	}
