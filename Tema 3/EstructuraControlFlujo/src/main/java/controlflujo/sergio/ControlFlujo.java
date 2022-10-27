99a6326a5e9c47f729ebb6ae1da15164bbb5ef36
Sergio García Vico <sergiogarciavicooficial@gmail.com>
2022-10-27 16:02:19 +0200

    Sergio

--- Tema 3/EstructuraControlFlujo/src/main/java/controlflujo/sergio/ControlFlujo.java
+++ Tema 3/EstructuraControlFlujo/src/main/java/controlflujo/sergio/ControlFlujo.java
@@ -10,44 +10,13 @@ public class ControlFlujo {
 		Scanner sc = new Scanner(System.in);
 
 		//Variables
+		boolean genero;
+		byte opcion;
 		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		/*System.out.println("Escribeme H si eres hombre o " +
+		System.out.println("Escribeme H si eres hombre o " +
 		"M si eres mujer");
 		
-		boolean genero = (sc.nextLine().equals("H")?true:false);
+		//boolean genero = (sc.nextLine().equals("H")?true:false);
 		
 		if(sc.nextLine().equals("H")) {
 			System.out.println("Eres hombre.");
@@ -61,12 +30,38 @@ public class ControlFlujo {
 		System.out.println("Escribeme una palabra que no sea \"Cermuzo\"");
 		String palabra = sc.nextLine();
 		
-		if(palabra.length() > 8) {
+		/*if(palabra.length() > 8) {
 			System.out.println("No me gustan las palabras tan largas.");
 		} else if(palabra.equals("cermuzo") || palabra.equals("Cermuzo")) {
 			System.out.println("Has escrito cermuzo, no escribas cermuzo, so cermuz" + (genero?"o":"a"));
 		} else {
 			System.out.println("Bonita palabra.");
 		} */
+		
+		System.out.println("Bienvenid" + (genero?"o":"a") +", " + "Has puesto la palabra: " +palabra + 
+				", ¿Que quieres hacer"  +  " con ella?" 
+				+ "\n\t1 - Invertir la palabra"
+				+ "\n\t2 - Codificar la palabra");
+		opcion = Byte.parseByte(sc.nextLine());
+		
+		switch(opcion) {
+			//Opcion 1 - Invertir la palabra
+			case 1:
+				System.out.println("Aqui pondre la palabra al reves");
+				break;
+			
+			//Opcion 2 - Codificar la palabra
+			case 2:
+				System.out.println("Aqui codificare la palabra");
+				break;
+				
+			//Opcion que se ejecuta si no has elegido 1 o 2
+			default:
+				System.out.println("Debiste elegir 1 o 2");
+				break;
+		}
+		
+		
+		
 		}	
 	}
