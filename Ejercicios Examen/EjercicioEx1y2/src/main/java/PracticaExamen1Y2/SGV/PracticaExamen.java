e8cc9bbe5d46eac9c8e03bc74b137075d5cfc7b8
Sergio García Vico <sergiogarciavicooficial@gmail.com>
2022-11-03 20:46:10 +0100

    Sergio

--- Ejercicios Examen/EjercicioEx1y2/src/main/java/PracticaExamen1Y2/SGV/PracticaExamen.java
+++ Ejercicios Examen/EjercicioEx1y2/src/main/java/PracticaExamen1Y2/SGV/PracticaExamen.java
@@ -1,12 +1,29 @@
 package PracticaExamen1Y2.SGV;
 
+import java.util.Scanner;
+
 public class PracticaExamen {
 
 	public static void main(String[] args) {
 		
+		Scanner sc = new Scanner(System.in);
+
+		//Variables
+		String nombre;
+		short añoNacimiento;
+		float peso;
 		
+		System.out.println("Dime tu nombre");
+		nombre = sc.nextLine();
+		System.out.println("Dime tu año de nacimiento");
+		añoNacimiento = Short.parseShort(sc.nextLine());
+		System.out.println("Dime tu peso (Kg)");
+		peso = Float.parseFloat(sc.nextLine());
 		
+		//Imprimir
+		System.out.println("¡Hola " + nombre + "!");
+		System.out.println("Han pasado " + (2022-añoNacimiento) + "desde que naci.");
+		System.out.println("Cumpliras 85 años en el año " + (añoNacimiento+85));
 		
-		
-	}
+	}	
 }
