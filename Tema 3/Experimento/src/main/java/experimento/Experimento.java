d5547d35a711e63025b5757bd94955b0c0368c77
Sergio García Vico <sergiogarciavicooficial@gmail.com>
2022-11-15 18:32:55 +0100

    Sergio

--- Tema 3/Experimento/src/main/java/experimento/Experimento.java
+++ Tema 3/Experimento/src/main/java/experimento/Experimento.java
@@ -1,63 +1,106 @@
 package experimento;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Experimento {
 
 	public static void main(String[] args) {
-		
-		//Scanner
+
 		Scanner sc = new Scanner(System.in);
-		
-		
+
 		System.out.println("Escribeme un texto, y le hacemos transformaciones " + "en el programa");
 
+		String codificado="";
+		String original;
+		String mimificado="";
+		String mimificadoOriginal = "";
 		String texto = sc.nextLine();
 		byte opcion;
 		
-		do {
-			System.out.println("Elige una opcion: " 
-			+ "\n\t0 - Salir"
-			+ "\n\t1 - Invertir la cadena"
-			+ "\n\t2 - Poner todo en mayusculas"
-			+ "\n\t3 - Poner todo en minusculas"
-			+ "\n\t4 - Poner mayuscula la primera letra, minuscula el resto"
-			+ "\n\t5 - Mimificar el texto"
-			+ "\n\t6 - Codificar el texto"
-			+ "\n\t7 - Decodificar el texto"
-			+ "\n\t8 - Codificar con caracteres aleatorios"
-			+ "\n\t9 - EXTRA (sin relacion) - Adivina que numero he pensado");
-			opcion=Byte.parseByte(sc.nextLine());
-		}while(opcion!=0);
+		/*
+		 * Ejercicio 5 a partir de la cadena mimificado usar un bucle para obtener la cadena original
+		 * Hacer el caso 8 | copypaste del caso 6 y entre letra y letra colocar una letra aleatoria entra a y z.
+		 */
 		
-		switch(opcion) {
-		case 1:
-			
-			break;
-		case 2:
-			
-			break;
-		case 3:
-			
-			break;
-		case 4:
-			
-			break;
-		case 5:
-			
-			break;
-		case 6:
-			
-			break;
-		case 7:
-			
-			break;
-		case 8:
-			
-			break;
-		case 9:
-			
-			break;
-		}
+		do {
+			System.out.println("Elige una opcion: " + "\n\t0 - Salir" + "\n\t1 - Invertir la cadena"
+					+ "\n\t2 - Poner todo en mayusculas" + "\n\t3 - Poner todo en minusculas"
+					+ "\n\t4 - Poner mayuscula la primera letra, minuscula el resto" + "\n\t5 - Mimificar el texto"
+					+ "\n\t6 - Codificar el texto" + "\n\t7 - Decodificar el texto"
+					+ "\n\t8 - Codificar con caracteres aleatorios"
+					+ "\n\t9 - EXTRA (sin relacion) - Adivina que numero he pensado");
+			opcion = Byte.parseByte(sc.nextLine());
+
+			switch (opcion) {
+			case 1:
+				for (short i = (short) (texto.length()-1); i >= 0; i--) {
+					System.out.print(texto.charAt(i));
+				}
+				System.out.println();
+				break;
+			case 2:
+				texto=texto.toUpperCase();
+				System.out.println(texto);
+				break;
+			case 3:
+				texto=texto.toLowerCase();
+				System.out.println(texto);
+				break;
+			case 4:
+				String resultado=""+texto.toUpperCase().charAt(0);
+				for(short i=1; i <texto.length(); i++) {
+					resultado+=texto.toLowerCase().charAt(i);
+				}
+				System.out.println(resultado);
+				break;
+			case 5:
+				for (short i = 0; i < texto.length(); i++) {
+					mimificado+=texto.charAt(i)+"mi";
+				}
+				System.out.println(mimificado);
+				
+				
+				for (short i = 0; i < mimificado.length(); i++) {
+					mimificado = mimificado.replaceAll("mi", "");
+				}
+				System.out.println(mimificado);
+				
+				break;
+			case 6:
+				codificado = "";
+				for (short i = 0; i < texto.length(); i++) {
+					codificado+=(char)(texto.charAt(i)+8);
+				}
+				System.out.println(codificado);
+				break;
+			case 7:
+				original = "";
+				for (short i = 0; i < codificado.length(); i++) {
+					original+=(char)(codificado.charAt(i)-8);
+				}
+				System.out.println(original);
+				break;
+			case 8:
+
+				break;
+			case 9:
+				Random aleatorio = new Random();
+				int numeroElegido = aleatorio.nextInt(1,11);
+				System.out.println("Adivina el numero entre (1 y 10)");
+				byte numeroEscogido;
+				do {
+					numeroEscogido = Byte.parseByte(sc.nextLine());
+					if(numeroEscogido != numeroElegido) {
+						System.out.println("Ese numero no era.");
+					}
+				}while(numeroEscogido != numeroElegido);
+				System.out.println("¡Has adivinado el numero!");
+				break;
+			}
+			System.out.println("\nPulsa intro para continuar o escribe 0 para salir");
+			sc.nextLine();
+		} while (opcion != 0);
+		System.out.println("Programa terminado");
 	}
 }
