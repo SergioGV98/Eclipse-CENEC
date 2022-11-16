a094de3c0e669a301ea96c2f3f54b64e1b8b4ddf
Sergio García Vico <sergiogarciavicooficial@gmail.com>
2022-11-16 12:00:05 +0100

    s

--- Tema 3/Experimento/src/main/java/experimento/Experimento.java
+++ Tema 3/Experimento/src/main/java/experimento/Experimento.java
@@ -10,19 +10,20 @@ public class Experimento {
 		Scanner sc = new Scanner(System.in);
 
 		System.out.println("Escribeme un texto, y le hacemos transformaciones " + "en el programa");
-		String codificado="";
+		String codificado = "";
 		String original;
-		String mimificado="";
-		String mimificado2="";
+		String mimificado = "";
+		String mimificado2 = "";
 		byte opcion;
-		
+
 		String texto = sc.nextLine();
-		
+
 		/*
-		 * Ejercicio 5 a partir de la cadena mimificado usar un bucle para obtener la cadena original
-		 * Hacer el caso 8 | copypaste del caso 6 y entre letra y letra colocar una letra aleatoria entra a y z.
+		 * Ejercicio 5 a partir de la cadena mimificado usar un bucle para obtener la
+		 * cadena original Hacer el caso 8 | copypaste del caso 6 y entre letra y letra
+		 * colocar una letra aleatoria entra a y z.
 		 */
-		
+
 		do {
 			System.out.println("Elige una opcion: " + "\n\t0 - Salir" + "\n\t1 - Invertir la cadena"
 					+ "\n\t2 - Poner todo en mayusculas" + "\n\t3 - Poner todo en minusculas"
@@ -34,76 +35,76 @@ public class Experimento {
 
 			switch (opcion) {
 			case 1:
-				for (short i = (short) (texto.length()-1); i >= 0; i--) {
+				for (short i = (short) (texto.length() - 1); i >= 0; i--) {
 					System.out.print(texto.charAt(i));
 				}
 				System.out.println();
 				break;
 			case 2:
-				texto=texto.toUpperCase();
+				texto = texto.toUpperCase();
 				System.out.println(texto);
 				break;
 			case 3:
-				texto=texto.toLowerCase();
+				texto = texto.toLowerCase();
 				System.out.println(texto);
 				break;
 			case 4:
-				String resultado=""+texto.toUpperCase().charAt(0);
-				for(short i=1; i <texto.length(); i++) {
-					resultado+=texto.toLowerCase().charAt(i);
+				String resultado = "" + texto.toUpperCase().charAt(0);
+				for (short i = 1; i < texto.length(); i++) {
+					resultado += texto.toLowerCase().charAt(i);
 				}
 				System.out.println(resultado);
 				break;
 			case 5:
 				for (short i = 0; i < texto.length(); i++) {
-					mimificado+=texto.charAt(i)+"mi";
+					mimificado += texto.charAt(i) + "mi";
 				}
 				System.out.println(mimificado);
-				
+
 				for (short i = 0; i < texto.length(); i++) {
-					mimificado2+=texto.charAt(i);
-					
-					//mimificado = mimificado.replaceAll("mi", "");
+					mimificado2 += texto.charAt(i);
+
+					// mimificado = mimificado.replaceAll("mi", "");
 				}
 				System.out.println(mimificado2);
-				
+
 				break;
 			case 6:
 				codificado = "";
 				for (short i = 0; i < texto.length(); i++) {
-					codificado+=(char)(texto.charAt(i)+8);
+					codificado += (char) (texto.charAt(i) + 8);
 				}
 				System.out.println(codificado);
 				break;
 			case 7:
 				original = "";
 				for (short i = 0; i < codificado.length(); i++) {
-					original+=(char)(codificado.charAt(i)-8);
+					original += (char) (codificado.charAt(i) - 8);
 				}
 				System.out.println(original);
 				break;
 			case 8:
 				codificado = "";
-				Random rnd = new Random();
-				char randomChar = (char) (rnd.nextInt(47,50));
+				Random letrasRandom = new Random();
 				for (short i = 0; i < texto.length(); i++) {
-					codificado+=(char)(texto.charAt(i)+8);
-					for(short j = 0; j < 1; j++) {
+					codificado += (char) (texto.charAt(i) + 8);
+					for (short j = 0; j < 1; j++) {
+						char randomChar = (char) (letrasRandom.nextInt(65, 91));
 						codificado += randomChar;
 					}
 				}
 				System.out.println(codificado);
 			case 9:
 				Random aleatorio = new Random();
-				int numeroElegido = aleatorio.nextInt(1,11);
+				int numeroElegido = aleatorio.nextInt(1, 11);
 				System.out.println("Adivina el numero entre (1 y 10)");
 				byte numeroEscogido;
 				do {
 					numeroEscogido = Byte.parseByte(sc.nextLine());
-					if(numeroEscogido != numeroElegido) {
+					if (numeroEscogido != numeroElegido) {
 						System.out.println("Ese numero no era.");
 					}
-				}while(numeroEscogido != numeroElegido);
+				} while (numeroEscogido != numeroElegido);
 				System.out.println("¡Has adivinado el numero!");
 				break;
 			}
