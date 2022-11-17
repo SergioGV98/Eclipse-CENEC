8498189efa6f6e419ef36ba930deb13a5e404377
Sergio García Vico <sergiogarciavicooficial@gmail.com>
2022-11-17 17:44:05 +0100

    sergio

---
+++ Tema 3/Funciones/src/main/java/funciones/Funciones.java
@@ -0,0 +1,11 @@
+package funciones;
+
+public class Funciones {
+
+	public static void funcionVoid() {
+		
+		System.out.println("Hola desde la funcion Void");
+		
+	}
+
+}
---
+++ Tema 3/Funciones/src/main/java/funciones/Main.java
@@ -0,0 +1,12 @@
+package funciones;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		//funcionVoid(); // Llamada desde la misma clase
+		Funciones.funcionVoid(); // Si la quisiera llamar desde otra clase
+
+	}
+
+}
