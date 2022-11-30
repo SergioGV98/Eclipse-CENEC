e19a34ab5b1c979ed4657a9cbffc5fcc92df6a61
Sergio García Vico <sergiogarciavicooficial@gmail.com>
2022-11-30 17:16:46 +0100

    Final de la relacion

--- Tema 3/Relacion/Ejercicio15/src/main/java/ejercicio15/Ejercicio15.java
+++ Tema 3/Relacion/Ejercicio15/src/main/java/ejercicio15/Ejercicio15.java
@@ -5,34 +5,42 @@ import java.util.Scanner;
 public class Ejercicio15 {
 
 	public static void main(String[] args) {
-	
-		
+
 		Scanner sc = new Scanner(System.in);
-		
+
 		String nombreBandaMetal = null;
-		byte eleccion;
+		byte eleccion = 5;
 		boolean salir = false;
-		
-		do {
-			System.out.println("¿Que quieres hacer?" + "\n\t0 - Salir" + "\n\t1 - Nombre banda de Metal" + "\n\t2 - Metalizar texto");
+
+
+		while (salir != true && (eleccion != 0 || eleccion != 1 || eleccion != 2)) {
+			System.out.println("¿Que quieres hacer?" + "\n\t0 - Salir" + "\n\t1 - Nombre banda de Metal"
+					+ "\n\t2 - Metalizar texto");
 			eleccion = Byte.parseByte(sc.nextLine());
 			
-		}while(eleccion < 0 || eleccion > 2);
-		
-		switch(eleccion) {
-		case 0:
-			salir = true;
-			break;
-		case 1:
-			System.out.println("Dime el nombre para tu banda");
-			nombreBandaMetal = sc.nextLine();
-			break;
-		case 2:
+			if(eleccion <0 || eleccion >2) {
+				System.out.println("Debes introducir un numero del 0 al 2");
+			}
 			
-			break;
+			switch (eleccion) {
+			case 0:
+				salir = true;
+				System.out.println("Vamos a terminar el prorama");
+				break;
+			case 1:
+				System.out.println("Dime el nombre para tu banda");
+				nombreBandaMetal = sc.nextLine();
+				nombreBandaMetal = " " + nombreBandaMetal + " ";
+				break;
+			case 2:
+				if(nombreBandaMetal == null) {
+					System.out.println("Debes introducir algo, para poder metalizar el texto.");
+					break;
+				} else if(nombreBandaMetal != null){
+					System.out.println(Funciones.metalizarNombreBanda(nombreBandaMetal));
+					break;
+				}
+			}
 		}
-		
-
 	}
-
 }
--- Tema 3/Relacion/Ejercicio15/src/main/java/ejercicio15/Funciones.java
+++ Tema 3/Relacion/Ejercicio15/src/main/java/ejercicio15/Funciones.java
@@ -5,11 +5,66 @@ import java.util.Random;
 public class Funciones {
 
 	public static String metalizarNombreBanda(String nombreBanda) {
-
 		Random simbolo = new Random();
-		String ret="";
+		String ret = "";
+		String terminado = "";
+		char randomChar = '0';
+		byte probabilidad = 0;
+		byte simboloFinal = -1;
+		char caracter = ' ';
 
-		
-		return ret;
+		for (short i = 0; i < nombreBanda.length(); i++) {
+			if (nombreBanda.charAt(i) == ' ') {
+				simboloFinal = (byte) (simbolo.nextInt(1, 5));
+				switch (simboloFinal) {
+				case 1:
+					caracter = '#';
+					ret += " " + caracter + " ";
+					break;
+				case 2:
+					caracter = '%';
+					ret += " " + caracter + " ";
+					break;
+				case 3:
+					caracter = '@';
+					ret += " " + caracter + " ";
+					break;
+				case 4:
+					caracter = '&';
+					ret += " " + caracter + " ";
+					break;
+				}
+			} else {
+				ret += nombreBanda.charAt(i);
+			}
+		}
+
+		for (short i = 0; i < ret.length(); i++) {
+			if (ret.charAt(i) == ' ') {
+				terminado += ret.charAt(i);
+			} else if (ret.charAt(i) != ' ') {
+				probabilidad = (byte) simbolo.nextInt(1, 101);
+				if (probabilidad <= 30) {
+					simboloFinal = (byte) simbolo.nextInt(1, 4);
+					switch (simboloFinal) {
+					case 1:
+						caracter = '1';
+						terminado += caracter;
+						break;
+					case 2:
+						caracter = '2';
+						terminado += caracter;
+						break;
+					case 3:
+						caracter = '3';
+						terminado += caracter;
+						break;
+					}
+				}
+				terminado += ret.charAt(i);
+			}
+		}
+
+		return terminado;
 	}
 }
