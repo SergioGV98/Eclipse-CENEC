bacbd9ced562c3a6242c79afd2527fe2e2461057
Sergio García Vico <sergiogarciavicooficial@gmail.com>
2022-12-15 17:47:25 +0100

    s

--- Tema 3/Relacion2/Ejercicio6/src/main/java/ejercicio6/Funciones.java
+++ Tema 3/Relacion2/Ejercicio6/src/main/java/ejercicio6/Funciones.java
@@ -6,7 +6,7 @@ public class Funciones {
 
 		
 		
-		for (byte i = 0; i < 127; i++) {
+		for (byte i = 1; i < 127; i++) {
 			
 			//OTro for dentro que te calcula como seria la cadena si a todos los
 			//numeros le restas i, si esa cadenacontiene quiero, entonces return i.
