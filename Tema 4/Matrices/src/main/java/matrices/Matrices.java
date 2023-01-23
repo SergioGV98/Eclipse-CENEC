faef9ad4040bf32c7e66ffb60f7426e446f0e3fd
Sergio García Vico <sergiogarciavicooficial@gmail.com>
2023-01-23 17:43:22 +0100

    s

--- Tema 4/Matrices/src/main/java/matrices/Matrices.java
+++ Tema 4/Matrices/src/main/java/matrices/Matrices.java
@@ -5,7 +5,25 @@ public class Matrices {
 	public static void main(String[] args) {
 
 		byte[][] matriz = new byte[3][3];
-
+		byte[][] matriz2 = {{5,3,22},
+							{4,44,1}};
+		
+		for(byte i = 0; i < matriz2.length; i++) {
+			for(byte j = 0; j < matriz2[i].length; j++) {
+				System.out.print(matriz2[i][j] + "\t");
+			}
+			System.out.println();
+		}
+		
+		System.out.println();
+		
+		//Imprime al reves la matriz
+		for(byte i = 0; i < matriz2.length; i++) {
+			for(byte j = (byte) (matriz2[i].length-1); j >= 0; j--) {
+				System.out.print(matriz2[i][j] + "\t");
+			}
+			System.out.println();
+		}
 	}
 
 }
