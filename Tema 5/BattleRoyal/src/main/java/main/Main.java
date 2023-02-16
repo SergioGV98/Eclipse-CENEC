d6b173b92d1a9c62d70691104d797e1890d5dc70
Sergio García Vico <sergiogarciavicooficial@gmail.com>
2023-02-16 17:48:58 +0100

    a

--- Tema 5/BattleRoyal/src/main/java/main/Main.java
+++ Tema 5/BattleRoyal/src/main/java/main/Main.java
@@ -11,17 +11,6 @@ public class Main {
 	public static void main(String[] args) {
 		
 		
-		/** Escudos **/
-		Escudo escudoObjetivo = new Escudo("Escudo objetivo", (byte) 10);
-		Escudo escudoConPinchos = new Escudo("Escudo con pinchos", (byte) 25);
-		Escudo escudoDeHawkwood = new Escudo("Escudo de Hawkwood", (byte) 34);
-		Escudo escudoCaballeroNegro = new Escudo("Escudo Caballero Negro", (byte) 52);
-		Escudo granEscudoDelCaballeroDeLothric = new Escudo("Gran escudo del caballero de Lothric", (byte) 65);
-		Escudo granEscudoDeGloria = new Escudo("Gran escudo de gloria", (byte) 78);
-		Escudo granEscudoDeYhorm = new Escudo("Gran escudo de Yhorm", (byte) 95);
-		Escudo granEscudoDeHavel = new Escudo("Gran escudo de Havel", (byte) 128);
-		
-		
 		Guerrero sergio = new Guerrero("Sergio", (byte) 100, laGranGuadañaDeFriede, escudoObjetivo);
 		Berserk carlos = new Berserk("Carlos", (byte) 100, latigoConMuescas, espadonDeEnlaceDeFuego);
 		
