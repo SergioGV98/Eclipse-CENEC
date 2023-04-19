24d64d16dbb0ebf101ee682007881cca09fa4fbb
Sergio García Vico <sergiogarciavicooficial@gmail.com>
2023-04-19 20:53:35 +0200

    Update Main.java

--- Tema 8/PruebasBaseDeDatos/src/main/java/main/Main.java
+++ Tema 8/PruebasBaseDeDatos/src/main/java/main/Main.java
@@ -4,11 +4,22 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 	
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Dime tu nombre");
+		String nombre = sc.nextLine();
+		System.out.println("Dime tu email");
+		String email = sc.nextLine();
+		System.out.println("Dime tu contraseña");
+		String password = sc.nextLine();
+		System.out.println("Dime tu telefono");
+		int telefono = Integer.parseInt(sc.nextLine());
+		
 		try {
 			Connection conexion = DriverManager.getConnection(
 					"jdbc:mysql://127.0.0.1:3306/bicicletasdaw", // Conexion a la base de datos
@@ -18,8 +29,9 @@ public class Main {
 			Statement smt = conexion.createStatement(); // Permite enviar consultas (Cursor)
 			
 			// Usar
-			smt.executeUpdate("insert into cliente values('sergio', 'sergio@gmail.com', 'sergio1234', 610844376)");
-			
+			String consulta = "insert into cliente values('"+nombre+"', '"+email+"', '"+password+"', "+telefono+")";
+			System.out.println(consulta);
+			smt.executeUpdate(consulta);
 			
 			smt.close(); // Cerramos la conexion del Statement
 			conexion.close(); // Cerramos la conexion a la base de datos
