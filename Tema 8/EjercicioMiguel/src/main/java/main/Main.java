2e21689ba0d9538c26b8dbbd971a0e711f79dbd0
Sergio García Vico <sergiogarciavicooficial@gmail.com>
2023-04-27 21:00:21 +0200

    Update Main.java

--- Tema 8/EjercicioMiguel/src/main/java/main/Main.java
+++ Tema 8/EjercicioMiguel/src/main/java/main/Main.java
@@ -1,6 +1,9 @@
 package main;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedHashSet;
 import java.util.Scanner;
 
 import utils.DAO;
@@ -15,11 +18,30 @@ public class Main {
 		System.out.println("Dime la contraseña");
 		String password = sc.nextLine();
 		
-		HashMap<String, Object> valoresBusqueda = new HashMap<String, Object>();
-		valoresBusqueda.put("email", email);
-		valoresBusqueda.put("password", password);
+		ArrayList<Object> cliente = null;
+		LinkedHashSet<String> consulta = new LinkedHashSet<String>();
+		consulta.add("nombre");
+		consulta.add("email");
+		consulta.add("telefono");
 		
-		HashMap<String, Object> busqueda = new HashMap<String, Object>();
-		busqueda.put("email", email);
+		HashMap<String, Object> restricciones = new HashMap<String, Object>();
+		restricciones.put("email", email);
+		restricciones.put("password", password);
+		try {
+			cliente = DAO.consultar("cliente", restricciones, consulta);
+			while(cliente.isEmpty()) {
+				System.out.println("Dime un email");
+				email = sc.nextLine();
+				restricciones.put("email", email);
+				System.out.println("Dime la contraseña");
+				password = sc.nextLine();
+				restricciones.put("password", password);
+				cliente = DAO.consultar("cliente", restricciones, consulta);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		System.out.println("¡Login correcto!");
+		System.out.println(cliente);
 	}
 }
