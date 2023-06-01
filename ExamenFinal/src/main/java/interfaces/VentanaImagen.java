350e6fe219708f38ed81a41de66c82d984256296
Sergio García Vico <sergiogarciavicooficial@gmail.com>
2023-06-01 17:58:36 +0200

    Update VentanaImagen.java

--- ExamenFinal/src/main/java/interfaces/VentanaImagen.java
+++ ExamenFinal/src/main/java/interfaces/VentanaImagen.java
@@ -12,70 +12,67 @@ import java.awt.event.MouseEvent;
 
 public class VentanaImagen extends JPanel {
 
-	private Ventana ventana;
+    private Ventana ventana;
+    private BufferedImage imagen;
 
-	public VentanaImagen(Ventana v, String ruta) {
-		this.ventana = v;
-		setLayout(new BorderLayout());
+    public VentanaImagen(Ventana v, String ruta) {
+        this.ventana = v;
+        setLayout(new BorderLayout());
 
-		ImageIcon imagen = cargarImagen(ruta);
-		if (imagen != null) {
-			JLabel background = new JLabel(imagen);
-			background.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
-			add(background, BorderLayout.CENTER);
-		}
+        cargarImagen(ruta);
 
-		JButton negativoImagenButton = new JButton("Ver negativo");
-		negativoImagenButton.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				ImageIcon imgNegativa = aplicarFiltroNegativo(ruta);
-				JPanel panel = new JPanel(new BorderLayout());
-				panel.add(new JLabel(imgNegativa), BorderLayout.CENTER);
-				JOptionPane.showMessageDialog(null, panel, "Imagen negativo", JOptionPane.PLAIN_MESSAGE);
-			}
-		});
-		negativoImagenButton.setFont(new Font("Tahoma", Font.PLAIN, 18));
-		add(negativoImagenButton, BorderLayout.SOUTH);
-	}
+        JButton negativoImagenButton = new JButton("Ver negativo");
+        negativoImagenButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                ImageIcon imgNegativa = aplicarFiltroNegativo(ruta);
+                JPanel panel = new JPanel(new BorderLayout());
+                panel.add(new JLabel(imgNegativa), BorderLayout.CENTER);
+                JOptionPane.showMessageDialog(null, panel, "Imagen negativo", JOptionPane.PLAIN_MESSAGE);
+            }
+        });
+        negativoImagenButton.setFont(new Font("Tahoma", Font.PLAIN, 18));
+        add(negativoImagenButton, BorderLayout.SOUTH);
+    }
 
-	public ImageIcon cargarImagen(String ruta) {
-		try {
-			BufferedImage img = ImageIO.read(new File(ruta));
-			int width = img.getWidth();
-			int height = img.getHeight();
-			setPreferredSize(new Dimension(width, height));
-			Image scaledImg = img.getScaledInstance(width, height, Image.SCALE_SMOOTH);
-			return new ImageIcon(scaledImg);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return null;
-	}
+    private void cargarImagen(String ruta) {
+        try {
+            imagen = ImageIO.read(new File(ruta));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-	public ImageIcon aplicarFiltroNegativo(String rutaImagen) {
-		try {
-			BufferedImage imgOriginal = ImageIO.read(new File(rutaImagen));
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        if (imagen != null) {
+            g.drawImage(imagen, 0, 0, getWidth(), getHeight(), this);
+        }
+    }
 
-			// Convertir la imagen en escala de grises
-			BufferedImage imgGrayscale = new BufferedImage(imgOriginal.getWidth(), imgOriginal.getHeight(),
-					BufferedImage.TYPE_BYTE_GRAY);
-			Graphics2D g2d = imgGrayscale.createGraphics();
-			g2d.drawImage(imgOriginal, 0, 0, null);
-			g2d.dispose();
+    public ImageIcon aplicarFiltroNegativo(String rutaImagen) {
+        try {
+            BufferedImage imgOriginal = ImageIO.read(new File(rutaImagen));
 
-			// Invertir los colores de la imagen en escala de grises para obtener el
-			// negativo
-			RescaleOp op = new RescaleOp(-1.0f, 255f, null);
-			BufferedImage imgNegativa = op.filter(imgGrayscale, null);
+            // Convertir la imagen en escala de grises
+            BufferedImage imgGrayscale = new BufferedImage(imgOriginal.getWidth(), imgOriginal.getHeight(),
+                    BufferedImage.TYPE_BYTE_GRAY);
+            Graphics2D g2d = imgGrayscale.createGraphics();
+            g2d.drawImage(imgOriginal, 0, 0, null);
+            g2d.dispose();
 
-			// Escalar la imagen negativa
-			Image scaledImg = imgNegativa.getScaledInstance(200, 150, Image.SCALE_SMOOTH);
-			return new ImageIcon(scaledImg);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return null;
-	}
+            // Invertir los colores de la imagen en escala de grises para obtener el negativo
+            RescaleOp op = new RescaleOp(-1.0f, 255f, null);
+            BufferedImage imgNegativa = op.filter(imgGrayscale, null);
+
+            // Escalar la imagen negativa
+            Image scaledImg = imgNegativa.getScaledInstance(200, 150, Image.SCALE_SMOOTH);
+            return new ImageIcon(scaledImg);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 
 }
